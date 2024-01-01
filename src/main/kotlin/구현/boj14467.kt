6e54f5d61644e3f8b94315004844b1ed1ec2578b
HaeTi <t9h9@naver.com>
2024-01-01 22:45:55 +0900

    [구현] 소가 길을 건너간 이유 1 구조분해선언, 배열에 데이터 넣기

--- src/main/kotlin/구현/boj14467.kt
+++ src/main/kotlin/구현/boj14467.kt
@@ -1,5 +1,23 @@
 package 구현
 
-fun main(){
-    println("hello")
+fun main() {
+    val observeNumber = readln().toInt()
+
+    val cowArr = Array(11) { IntArray(1) { -1 } }
+    var cowSum = 0
+
+    for (i in 0 until observeNumber) {
+        val (cowNum, cowLocate) = readln().split(" ").map { it.toInt() }
+
+        if (cowArr[cowNum][0] == -1) {
+            cowArr[cowNum][0] = cowLocate
+        } else {
+            if (cowArr[cowNum][0] != cowLocate) {
+                cowSum++
+                cowArr[cowNum][0] = cowLocate
+            }
+        }
+    }
+
+    println(cowSum)
 }
